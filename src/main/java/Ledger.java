114df41171bf23bf81aed94ad629f90b91f2af5b
DomaniqueK <domaniqueking24@gmail.com>
2025-10-10 15:57:43 -0500

    Made declarations for the transactions.csv, had to go back and add times to the csv also

--- src/main/java/Ledger.java
+++ src/main/java/Ledger.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Ledger {
     public static void main(String[] args) {
+
+        ArrayList<Transactions> ledger = new ArrayList<>();
+
+        String description;
+        String type;
+        double amount;
+        LocalDate date;
+        LocalTime time;
 
     }
 }
