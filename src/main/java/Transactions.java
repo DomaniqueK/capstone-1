c3d582203691823e06b0770137ebf5bc6813a39d
DomaniqueK <domaniqueking24@gmail.com>
2025-10-10 13:53:57 -0500

    Created a transaction class and made steps to get started

---
+++ src/main/java/Transactions.java
@@ -0,0 +1,10 @@
+public class Transactions {
+
+    // Steps
+    // Create transactions.csv file
+    // Open file in the bufferedreader
+    // Read the header
+    // Break it up in parts
+    // Use parts to instantiate ledger object??
+
+}
