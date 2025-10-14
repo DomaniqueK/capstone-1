25b14341c3b01c836e95861e358cc8478e8b8747
DomaniqueK <domaniqueking24@gmail.com>
2025-10-14 09:13:41 -0500

    Readded the scanner, ArrayList, and try and catch to load transactions and ran it and received no errors. I'm breaking everything down now

--- src/main/java/LedgerApp.java
+++ src/main/java/LedgerApp.java
@@ -1,13 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LedgerApp {
-    public static ArrayList<Transactions> transactions = new ArrayList<>();
-    public static Scanner scanner = new Scanner(System.in);
-}
-    // Steps
-    // Create transactions.csv file
-    // Open file in the bufferedReader
-    // Read the header
-    // Break it up in parts
-    // Use parts to instantiate ledger object??
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Transactions> transactions = new ArrayList<>();
+
+    public static void main(String[] args) {
+    }
+
+    public static void loadtransaction(String[] args) {
+
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            bufferedReader.readLine();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found " + e);
+        } catch (IOException e) {
+            System.out.println("Please select another option " + e);
+        }
+    }
+    }
